b60c28d9afa5cea57b7c18ea37d8febb4d1f7863
SALL <sallibrahima1992@gmail.com>
2021-05-12 11:23:50 +0200

    Premiere fonction d'affichage de tableau

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
+import { Console } from 'console';
 import express from 'express';
 const app = express();
 const port = 3000;
+const nbaplayers=['Wiggins','Curry','Lebron','James','Paul','Zion','Brandon','Ingram','Anthony','Davis','Jimmy','Butler']
+function show(array){
+  return array.reduce(function(memo, val) {
+    return memo + ' , '+val;
+  });
+}
 app.get('/', (req, res) => {
-  res.send('The sedulous hyena ate the antelope!');
+  //res.send('The sedulous hyena ate the antelope!');
+  res.send(show(nbaplayers));
 });
 // start the Express server
 app.listen( port, () => {
