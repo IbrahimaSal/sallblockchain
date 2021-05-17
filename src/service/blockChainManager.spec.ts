17cdf3ebf8db9d327744dcdceb5b5514d5921ebc
SALL <sallibrahima1992@gmail.com>
2021-05-17 17:04:33 +0200

    code review

--- src/service/blockChainManager.spec.ts
+++ src/service/blockChainManager.spec.ts
@@ -1,9 +1,9 @@
 import {
   sha256, createABlockChain, createBlock, getLastBlock,
-} from '../service/blockChainManager';
+} from './blockChainManager';
 
-describe('Test de ma fonction de hachage', () => {
-  it('mon deuxieme test', () => {
+describe('sha256', () => {
+  it(' returns the result encryption code of 0', () => {
     // given
     const zero = sha256(0);
     // when
@@ -15,19 +15,19 @@ describe('Test de ma fonction de hachage', () => {
 describe('createBlock', () => {
   it('returns a genesis block (without previous blockId) when index is zero', () => {
     // given
-    const genesisBlock = { position: '0', id: '5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9' };
+    const expectedGenesisBlock = { position: '0', id: '5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9' };
     // when
-    const genBlock = createBlock('iba', 0);
+    const result = createBlock('iba', 0);
     // then
-    expect(genBlock).toStrictEqual(genesisBlock);
+    expect(result).toStrictEqual(expectedGenesisBlock);
   });
   it('returns a blocks (with a previousId) ', () => {
     // given
     const expectedBlock = { position: '1', id: '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b', previousBlockId: '5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9' };
     // when
-    const genBlock = createBlock('iba', 1);
+    const result = createBlock('iba', 1);
     // then
-    expect(expectedBlock).toStrictEqual(genBlock);
+    expect(result).toStrictEqual(expectedBlock);
   });
 });
 
@@ -44,9 +44,9 @@ describe('createABlockChain', () => {
     // given
     const expectedBlockChain = [{ position: '0', id: '5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9' }, { position: '1', id: '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b', previousBlockId: '5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9' }, { position: '2', id: 'd4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35', previousBlockId: '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b' }, { position: '3', id: '4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce', previousBlockId: 'd4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35' }];
     // when
-    const genBlockChain = createABlockChain(4);
+    const result = createABlockChain(4);
     // then
-    expect(expectedBlockChain).toStrictEqual(genBlockChain);
+    expect(expectedBlockChain).toStrictEqual(result);
   });
 });
 describe('getLastBlock', () => {
@@ -55,17 +55,17 @@ describe('getLastBlock', () => {
     // given
     const expectedGenesisBlock = { position: '0', id: '5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9' };
     // when
-    const genBlock = getLastBlock(blockChain);
+    const result = getLastBlock(blockChain);
     // then
-    expect(genBlock).toStrictEqual(expectedGenesisBlock);
+    expect(result).toStrictEqual(expectedGenesisBlock);
   });
   it('returns the last element(with previousBlockId) of (multi-element) blockchain', () => {
-    const testedBlockChain = [{ position: '0', id: '5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9' }, { position: '1', id: '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b', previousBlockId: '5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9' }, { position: '2', id: 'd4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35', previousBlockId: '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b' }, { position: '3', id: '4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce', previousBlockId: 'd4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35' }];
     // given
+    const testedBlockChain = [{ position: '0', id: '5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9' }, { position: '1', id: '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b', previousBlockId: '5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9' }, { position: '2', id: 'd4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35', previousBlockId: '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b' }, { position: '3', id: '4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce', previousBlockId: 'd4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35' }];
     const expectedLastBlock = { position: '3', id: '4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce', previousBlockId: 'd4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35' };
     // when
-    const genLastBlock = getLastBlock(testedBlockChain);
+    const result = getLastBlock(testedBlockChain);
     // then
-    expect(expectedLastBlock).toStrictEqual(genLastBlock);
+    expect(expectedLastBlock).toStrictEqual(result);
   });
 });
