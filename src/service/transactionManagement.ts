c90db4e7c5e81dda8fefc94a574955b772c8f5fb
SALL <sallibrahima1992@gmail.com>
2021-05-26 03:50:44 +0200

    refactor transaction management

--- src/service/transactionManagement.ts
+++ src/service/transactionManagement.ts
@@ -10,7 +10,7 @@ export const createTransaction = (amount:number, sender:user, receiver:user, sta
   {
     amount, sender, receiver, status,
   });
-const removeTransaction = (array:transaction[], element:transaction) => (array.filter((tr) => (tr !== element)));
+// const removeTransaction = (array:transaction[], element:transaction) => (array.filter((tr) => (tr !== element)));
 
 export const getBalance = (BlockChain:blockChain, User:user) : number => {
   let result = 0;
@@ -25,22 +25,19 @@ export const getBalance = (BlockChain:blockChain, User:user) : number => {
   return result;
 };
 
-const isTransactionValid = (BlockChain:blockChain, Transaction:transaction):boolean => {
-  if (Transaction.amount <= 0) {
-    return false;
-  }
-  if (getBalance(BlockChain, Transaction.sender) < Transaction.amount) {
-    return false;
-  }
-  return true;
-};
+const isTransactionValid = (BlockChain:blockChain, Transaction:transaction):boolean => !(
+  (Transaction.amount <= 0) || (getBalance(BlockChain, Transaction.sender) < Transaction.amount));
+
 export const minePendingExchangeTransaction = (BlockChain:blockChain, Block : block) : block => {
   const newPendingTransactions = Block.pendingTransactions.filter((Transaction) => (isTransactionValid(BlockChain, Transaction)));
-  newPendingTransactions.forEach((Tr) => {
-    console.log(`${Tr.sender.privateKey} has transferred ${Tr.amount} to ${Tr.receiver.privateKey}`);
-    return BlockChain.transactions.push(createTransaction(Tr.amount, Tr.sender, Tr.receiver, statusType.achieved));
+  newPendingTransactions.forEach((Transaction) => {
+    console.log(`${Transaction.sender.privateKey} has transferred ${Transaction.amount} to ${Transaction.receiver.privateKey}`);
+    return BlockChain.transactions.push(createTransaction(Transaction.amount, Transaction.sender, Transaction.receiver, statusType.achieved));
   });
-  Block.pendingTransactions = Block.pendingTransactions.filter((Tr) => ((!newPendingTransactions.includes(Tr)) || (Tr.sender === null || Tr.receiver === null)));
+  Block.pendingTransactions = Block.pendingTransactions.filter(
+    (Transaction) => ((!newPendingTransactions.includes(Transaction))
+    || (Transaction.sender === null || Transaction.receiver === null)),
+  );
   return Block;
 };
 const findMatchingTransaction = (BlockChain:blockChain, Block : block, Transaction :transaction) :transaction => {
@@ -52,25 +49,55 @@ const findMatchingTransaction = (BlockChain:blockChain, Block : block, Transacti
   }
   return undefined;
 };
-export const minePendingBuyOrSellTransaction = (BlockChain:blockChain, Block : block) : block => {
+/* export const minePendingBuyOrSellTransaction = (BlockChain:blockChain, Block : block) : block => {
   const filteredBlockPendingTransactionArray = Block.pendingTransactions.filter(
     (tr) => ((tr.sender === null || tr.receiver)
-      && (findMatchingTransaction(BlockChain, Block, tr) !== undefined || findMatchingTransaction(BlockChain, Block, tr) !== null)),
+      && (findMatchingTransaction(BlockChain, Block, tr) !== undefined
+      || findMatchingTransaction(BlockChain, Block, tr) !== null)),
   );
+  console.log(filteredBlockPendingTransactionArray);
   for (const Transaction of filteredBlockPendingTransactionArray) {
     const Match = findMatchingTransaction(BlockChain, Block, Transaction);
     if (Transaction.sender === null) {
       console.log(`${Match.sender.privateKey} has transferred ${Match.amount} to ${Transaction.receiver.privateKey}`);
       BlockChain.transactions.push(createTransaction(Match.amount, Match.sender, Transaction.receiver, statusType.achieved));
-      Block.pendingTransactions = removeTransaction(Block.pendingTransactions, Transaction);
-      Block.pendingTransactions = removeTransaction(Block.pendingTransactions, Match);
+      Block.pendingTransactions = Block.pendingTransactions.filter((transactionA) => (Transaction !== transactionA && Match !== transactionA));
     }
     if (Transaction.receiver === null) {
       console.log(`${Transaction.sender.privateKey} has transferred ${Match.amount} to ${Match.receiver}`);
       BlockChain.transactions.push(createTransaction(Match.amount, Transaction.sender, Match.receiver, statusType.achieved));
-      Block.pendingTransactions = removeTransaction(Block.pendingTransactions, Transaction);
-      Block.pendingTransactions = removeTransaction(Block.pendingTransactions, Match);
+      Block.pendingTransactions = Block.pendingTransactions.filter((transactionA) => (Transaction !== transactionA && Match !== transactionA));
     }
   }
   return Block;
+}; */
+export const minePendingBuyOrSellTransaction = (BlockChain:blockChain, Block : block) : block => {
+  for (const Transaction of Block.pendingTransactions) {
+    const Match = findMatchingTransaction(BlockChain, Block, Transaction);
+    if (Transaction.receiver === null && Match !== null && Match !== undefined) {
+      console.log(`${Transaction.sender.privateKey} has transferred ${Match.amount} to ${Match.receiver.privateKey}`);
+      BlockChain.transactions.push(createTransaction(Match.amount, Transaction.sender, Match.receiver, statusType.achieved));
+      Block.pendingTransactions = Block.pendingTransactions.filter((transactionA) => (Transaction !== transactionA && Match !== transactionA));
+    } else if (Transaction.sender === null && Match !== null && Match !== undefined) {
+      console.log(`${Match.sender.privateKey} has transferred ${Match.amount} to ${Transaction.receiver.privateKey}`);
+      BlockChain.transactions.push(createTransaction(Match.amount, Match.sender, Transaction.receiver, statusType.achieved));
+      Block.pendingTransactions = Block.pendingTransactions.filter((transactionA) => (Transaction !== transactionA && Match !== transactionA));
+    }
+  }
+  console.log(Block);
+  return Block;
 };
+
+// export const minePendingBuyOrSellTransaction = (BlockChain:blockChain, Block : block) : block => {
+//   Block.pendingTransactions
+//     .filter((transaction) => findMatchingTransaction(BlockChain, Block, transaction))
+//     .forEach((transaction) => {
+//       console.log(`${transaction.sender.privateKey} has transferred ${matchingTransaction.amount} to ${matchingTransaction.receiver.privateKey}`);
+//       transaction.sender ?
+//         BlockChain.transactions.push(createTransaction(matchingTransaction.amount, transaction.sender, matchingTransaction.receiver, statusType.achieved)) : 
+//         BlockChain.transactions.push(createTransaction(matchingTransaction.amount, matchingTransaction.sender, transaction.receiver, statusType.achieved));
+//       Block.pendingTransactions = Block.pendingTransactions.filter((transactionA) => (transaction !== transactionA && matchingTransaction !== transactionA));
+//     });
+
+//   return Block;
+// };
