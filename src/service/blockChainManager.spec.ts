15098dc6959a60467bec83be0b3f8800f16ff319
SALL <sallibrahima1992@gmail.com>
2021-07-06 10:36:38 +0200

    test

--- src/service/blockChainManager.spec.ts
+++ src/service/blockChainManager.spec.ts
@@ -12,7 +12,7 @@ import { block } from '../model/block';
 import { statusType, transaction } from '../model/transaction';
 import { user } from '../model/user';
 import { createTransaction, getBalance } from './transactionManagement';
-import { scanBlockChainUserTable, scanTable2 } from './Database';
+// import { scanBlockChainUserTable, scanTable2 } from './Database';
 
 describe('sha256', () => {
   it(' returns the result encryption code of 0', () => {
@@ -134,24 +134,24 @@ describe('mine', () => {
     expect(getBalance(blockChain, secondMiner)).toStrictEqual(100);
   });
 });
-describe('scantable2', () => {
-  it(' returns the result encryption code of 0', async () => {
-    // given
-    const tablename = 'BlockChainUsers';
-    // when
-    const result = await scanTable2(tablename);
-    // then
-    expect(result.length).toStrictEqual(7);
-  });
-});
+// describe('scantable2', () => {
+//   it(' returns the result encryption code of 0', async () => {
+//     // given
+//     const tablename = 'BlockChainUsers';
+//     // when
+//     const result = await scanTable2(tablename);
+//     // then
+//     expect(result.length).toStrictEqual(7);
+//   });
+// });
 
-describe('scanBlockChainUserTable', () => {
-  it(' returns the result encryption code of 0', async () => {
-    // given
-    const tablename = 'BlockChainUsers';
-    // when
-    const result = await scanBlockChainUserTable(tablename);
-    // then
-    expect(result.length).toStrictEqual(7);
-  });
-});
+// describe('scanBlockChainUserTable', () => {
+//   it(' returns the result encryption code of 0', async () => {
+//     // given
+//     const tablename = 'BlockChainUsers';
+//     // when
+//     const result = await scanBlockChainUserTable(tablename);
+//     // then
+//     expect(result.length).toStrictEqual(7);
+//   });
+// });
